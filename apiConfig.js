14266051f82fb33f1653bdad23d69cb8557601f9
ennepiva <enne.piva2@gmail.com>
2024-08-25 18:26:57 +0200

    test

--- apiConfig.js
+++ apiConfig.js
@@ -2,7 +2,6 @@
 
 export const discogsApiHeaders = {
     'Authorization': 'Discogs token=',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.1; SM-E500L Build/KTU84P) AppleWebKit/603.7 (KHTML, like Gecko)  Chrome/47.0.2363.266 Mobile Safari/600.7'
-};
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 8_2_4) Gecko/20100101 Firefox/49.9'
 
 export const youtubeApiKey = '';
